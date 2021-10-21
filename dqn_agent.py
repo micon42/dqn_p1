fef0c6af807902cde6217ddbb396a6f81722e5a9
Frank <f.suhling@gmx.de>
2021-10-21 18:20:57 +0200

    Cosmetics

--- dqn_agent.py
+++ dqn_agent.py
@@ -86,7 +86,12 @@ class Agent():
         states, actions, rewards, next_states, dones = experiences
 
         # Get max predicted Q values (for next states) from target model
-        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        # Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+
+        # DDQN
+        next_actions = torch.argmax(self.qnetwork_local(next_states), 1, keepdim=True)
+        Q_targets_next = self.qnetwork_target(next_states).gather(1, next_actions)
+
         # Compute Q targets for current states 
         Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
 
